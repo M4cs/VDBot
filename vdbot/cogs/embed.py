6546a5e640b96d7f0b6a4ba2d634196e30d6577d
M4cs <mabridgland@protonmail.com>
2021-04-10 19:39:34 -0400

    I should lint

--- vdbot/cogs/embed.py
+++ vdbot/cogs/embed.py
@@ -1,7 +1,12 @@
 from vdbot import bot
 from discord import Embed
 
+allowed_users = [368083908862017537, 830587634110693388, 524039295317835797]
+
 @bot.command(aliases=['embed', 'e'], help="Embed a message")
 async def on_embed(ctx, title, *, text):
-    embedVar = Embed(title=title, description=text, color=0x000000)
-    await ctx.channel.send(content="", embed=embedVar)
+    if ctx.message.author.id in allowed_users:
+        embedVar = Embed(title=title, description=text, color=0x000000)
+        await ctx.channel.send(content="", embed=embedVar)
+    else:
+        await ctx.channel.send(content="**You do not have permission to use that command**")

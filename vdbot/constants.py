db993c109413b12a9bbedf193f9125d44d56cab4
M4cs <mabridgland@protonmail.com>
2021-04-11 12:14:38 -0400

    Add Constants

---
+++ vdbot/constants.py
@@ -0,0 +1,19 @@
+WELCOME_MSG = """
+Our goal is to have a safe space for developers to come chat, mess around, and learn from each other. Before you start, we ask that you take a look at the rules section and familiarize yourself with it.
+
+You can find the rules here: <#830577877761654794>
+
+If you are a mod creator or asset creator and would like access to the dev/asset chats, head to <#830561120456278078> and react with the role you want to receive!
+
+You also have the ability to use some commands in the server. The following commands are available to all members:
+
+**!define <word>** - Define a word quickly and easily
+
+**!howdoi <query>** - Find code snippets from StackOverflow
+
+**!wheretostart** - Find links to the Valheim Modding Wiki Easily
+
+**!mod <query>** - Search for Mods on Thunderstore and Nexusmods
+
+**!help** - Get the available commands for the bot!
+"""

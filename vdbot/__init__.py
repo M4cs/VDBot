11766449b0bcc94187b0b0def34ec3ee3dd42436
M4cs <mabridgland@protonmail.com>
2021-04-11 12:13:08 -0400

    Update Help

--- vdbot/__init__.py
+++ vdbot/__init__.py
@@ -55,16 +55,16 @@ async def command_logger(ctx):
 
 @bot.command('help')
 async def on_help(ctx):
-    async with ctx.channel.typing():
-        embed = Embed(title="Help Menu", description="Bot Prefix: !")
-        embed.add_field(name="!define <word>", value="Define a word quickly from the dictionary")
-        embed.add_field(name="!invite", value="Get the permanent invite link")
-        embed.add_field(name="!howdoi <your query>", value="Get a StackOverflow snippet by query")
-        embed.add_field(name="!mod <mod title>", value="Find links to mods based on a title")
-        embed.add_field(name="!wheretostart", value="Find links to the Valheim Modding Wiki easily")
-        embed.add_field(name="!template", value="Get an easy plugin class template to copy from")
-        embed.add_field(name="!info", value="Get information about the bot")
-    await ctx.reply(embed=embed)
+    await ctx.reply("Check your DMs!")
+    embed = Embed(title="Help Menu", description="Bot Prefix: !")
+    embed.add_field(name="!define <word>", value="Define a word quickly from the dictionary")
+    embed.add_field(name="!invite", value="Get the permanent invite link")
+    embed.add_field(name="!howdoi <your query>", value="Get a StackOverflow snippet by query")
+    embed.add_field(name="!mod <mod title>", value="Find links to mods based on a title")
+    embed.add_field(name="!wheretostart", value="Find links to the Valheim Modding Wiki easily")
+    embed.add_field(name="!template", value="Get an easy plugin class template to copy from")
+    embed.add_field(name="!info", value="Get information about the bot")
+    await ctx.message.author.send(embed=embed)
 
     
 def run_bot():

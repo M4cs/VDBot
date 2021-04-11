5528a0fc7d1670365dbdd79fb249c6f644e3246a
M4cs <mabridgland@protonmail.com>
2021-04-10 20:31:16 -0400

    Fixes

--- vdbot/__init__.py
+++ vdbot/__init__.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext.commands import Bot
+from discord.ext.commands.help import MinimalHelpCommand
 from vdbot.config import config
 from PyDictionary import PyDictionary
 from discord import Embed, activity
@@ -26,7 +27,7 @@ class Statistics:
 
 def create_bot():
     dictionary = PyDictionary()
-    bot = Bot("!", help_command=None)
+    bot = Bot("!")
     console = Console(width=80)
     stats = Statistics()
     return bot, dictionary, console, stats
@@ -45,16 +46,10 @@ bot.add_listener(on_ready)
 
 @bot.before_invoke
 async def command_logger(ctx):
-    if ctx.command.name == "help":
-        await ctx.message.delete()
     stats.command_count += 1
     console.print(Panel('[bold green]Called Command'), justify='center')
     console.print(Panel('[bold green]Command: [/bold green]' + ctx.command.name + '\n[bold green] Possible Aliases: [/bold green]' + str(ctx.command.aliases)), justify='center')
 
-@bot.command("help")
-async def on_help(ctx):
-    embed = Embed(title="Help Menu", description="!define WORD - Define a word\n!howdoi QEURY - Get a stackoverflow answer quickly (ex !howdoi for loop python)")
-    await ctx.channel.send()
 
 def run_bot():
     try:

ae929eb4ba7bb4c61b2fd2e572ede4150b619291
M4cs <mabridgland@protonmail.com>
2021-04-11 01:00:47 -0400

    Thunderstore Indexing

--- vdbot/cogs/mods.py
+++ vdbot/cogs/mods.py
@@ -1,14 +1,26 @@
 from discord.embeds import Embed
 from vdbot import bot, mods, console
+from discord.ext import commands
 
 @bot.command(aliases=['mod', 'findmod'])
 async def on_find_mods(ctx, *, query):
-    async with ctx.channel.typing():
-        results = mods.find_mods(query)
-        embed = Embed(title="Mod Results:", description="Here's what I could find on Nexusmods (TS Coming Soon):")
-        for result in results:
-            embed.add_field(name=result['name'],
-            value="**Summary**\n" + result['data']['summary'] + "\n**Link:** " + result['data']['url'], inline=False)
-    await ctx.reply(embed=embed)
+    if not mods.is_reindexing:
+        async with ctx.channel.typing():
+            results = mods.find_mods(query)
+            embed = Embed(title="Mod Results:", description="Here's what I could find on Nexusmods and Thunderstore")
+            for result in results:
+                embed.add_field(name=result['name'],
+                value="**Summary**\n" + result['data']['summary'] + "\n**Nexus Link:** " + result['data']['url'], inline=False)
+        await ctx.reply(embed=embed)
+    else:
+        await ctx.reply(content="**I'm reindexing right now, try again in 10 seconds!**")
         
+@bot.command(aliases=['updateindex'])
+async def on_update_index(ctx):
+    if ctx.message.author.id in [368083908862017537, 830587634110693388, 524039295317835797]:
+        async with ctx.channel.typing():
+            await ctx.reply(content="Updating Indexes...")
+            mods.update_index()
+        await ctx.reply(content="Updated Indexes!")
+
 
--- vdbot/modfinder/__init__.py
+++ vdbot/modfinder/__init__.py
@@ -1,12 +1,19 @@
 from fuzzywuzzy import process
-import json
+import json, requests
 
 class ModFinder:
     def __init__(self):
+        self.is_reindexing = False
         self.mods = None
         with open('mods.json', 'r') as f:
             self.mods = json.loads(f.read())
         self.mod_titles = self.mods.keys()
+        res = requests.get('https://valheim.thunderstore.io/api/v1/package').json()
+        for mod in res:
+            if mod['name'] in self.mod_titles:
+                self.mods[mod['name']]['url'] += "\n**TS Link:** " + mod['package_url']
+            else:
+                self.mods[mod['name']] = {'summary': mod['versions'][-1]['description'], 'url': mod['package_url']}
     
     def find_mods(self, query):
         results = process.extract(query, self.mod_titles, limit=3)
@@ -14,6 +21,19 @@ class ModFinder:
         for result in results:
             mods.append({'name': result[0], 'data': {**self.mods[result[0]]}})
         return mods
-        
+
+    def update_index(self):
+        self.is_reindexing = True
+        self.mods = None
+        with open('mods.json', 'r') as f:
+            self.mods = json.loads(f.read())
+        self.mod_titles = self.mods.keys()
+        res = requests.get('https://valheim.thunderstore.io/api/v1/package').json()
+        for mod in res:
+            if mod['name'] in self.mod_titles:
+                self.mods[mod['name']]['url'] += "\n**TS Link:** " + mod['package_url']
+            else:
+                self.mods[mod['name']] = {'summary': mod['versions'][-1]['description'], 'url': mod['package_url']}
+        self.is_reindexing = False
         
     

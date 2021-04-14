b9f08af7e8c0e767975d9c0c0ccf0380c1c2c98d
M4cs <mabridgland@protonmail.com>
2021-04-13 21:24:32 -0400

    Lol silly max

--- vdbot/event_listeners.py
+++ vdbot/event_listeners.py
@@ -24,7 +24,7 @@ async def on_ready():
 @bot.event
 async def on_message(ctx):
     if ctx.author.id != bot.user.id:
-        if any(x for x in ['private', 'public', 'int', 'string', 'static', ');', '{\n', 'using', ';\n']) in ctx.content and '```' in ctx.content and '```cs\n' not in ctx.content:
+        if any(x in ctx.content for x in ['private', 'public', 'int', 'string', 'static', ');', '{\n', 'using', ';\n']) and '```' in ctx.content and '```cs\n' not in ctx.content:
             await ctx.reply(embed=Embed(title="Be A Homie", description="Use syntax highlighting! You can do so by adding code blocks with the language, like this:\n\n\\```cs\n\n\\```"))
     await bot.process_commands(ctx)
     stats.msgs_parsed += 1
